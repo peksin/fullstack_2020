05f6757533211b2386b99e30fdfc94fa1efab8bc
Pekka Sinkkonen <peksin@protonmail.com>
2020-07-16 17:44:59 +0300

    teht 1.11

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -12,9 +12,12 @@ const Button = ({ onClick, text }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      {text} {value}
-    </div>
+    <>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+    </>
   )
 }
 
