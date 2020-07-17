761f7fc99cd30dde93a2a687897a20287132ba56
Pekka Sinkkonen <peksin@protonmail.com>
2020-07-17 13:34:08 +0300

    teht 2.5 ja 2.6 oikeasti

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+
+const Person = ({ person }) => {
+  return (
+    <>
+      {person.name} <br/>
+    </>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    // luodaan olio uutta puhelinnumeroa varten
+    const nameObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+                  value={newName}
+                  onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map((person) => 
+          <Person person={person} />)}
+    </div>
+  )
+
+}
+
+export default App
