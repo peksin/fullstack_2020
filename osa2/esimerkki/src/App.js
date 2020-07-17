da055d6d5d5dea2efd942baac1b37933bfb62f28
Pekka Sinkkonen <peksin@protonmail.com>
2020-07-17 13:33:37 +0300

    osa2 teht 2.5 ja 2.6

--- osa2/esimerkki/src/App.js
+++ osa2/esimerkki/src/App.js
@@ -1,15 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  // tapahtumankasittelija onSubmitille
+  const addNote = (event) => {
+    event.preventDefault()
+    // luodaan olio uutta muistiinpanoa varten
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+
+    // lisataan oikeaoppisesti taulukon jatkoksi
+    setNotes(notes.concat(noteObject))
+    // tyhjentaa syotekenttaa kontrolloivan tilan
+    setNewNote('')
+  }
+
+  // tapahtumankasittelija lomakkeen input-komponentin
+  // muutokselle
+  const handleNoteChange = (event) => {
+    console.log(event.target.value) // viittaa syotekentan arvoon
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map((note, i) => 
-          <Note key={note.id} note={note} />
+        {notesToShow.map((note, i) => 
+          <Note key={i} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
