27f0d7e4212d1ef6a5d020c1d54b410cd1e928c4
Pekka Sinkkonen <peksin@protonmail.com>
2020-07-18 11:59:42 +0300

    osa2 teht 2.9 alkuun

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -10,11 +10,14 @@ const Person = ({ person }) => {
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1231244' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
 
   // kasittelee vain lomakkeen kentan muutosta
   const handleNameChange = (event) => {
@@ -23,6 +26,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleNewFilter = (event) => {
+    setNewFilter(event.target.value)
   }
 
   // hoitaa puhelinnumeron lisayksen
@@ -41,11 +48,21 @@ const App = () => {
     : window.alert(`${newName} is already added to phonebook`)
 
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input
+            value={newFilter}
+            onChange={handleNewFilter} />
+        </div>
+      </form>
+
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input 
