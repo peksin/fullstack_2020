60e53f1ee44a35522e5425945a848689dd3aab9a
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-01 13:28:31 +0300

    osa 2 luku c esimerkki

--- osa2/esimerkki/src/App.js
+++ osa2/esimerkki/src/App.js
@@ -1,25 +1,21 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
-  const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
-  }
+  }, [])
   
-  // toisena parametrina tyhja taulukko, eli efekti suoritetaan vain 
-  // komponentin ensimmaisen renderoinnin jalkeen
-  useEffect(hook, [])
+
 
   // tapahtumankasittelija onSubmitille
   const addNote = (event) => {
@@ -29,13 +25,14 @@ const App = (props) => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
-      id: notes.length + 1,
     }
 
-    // lisataan oikeaoppisesti taulukon jatkoksi
-    setNotes(notes.concat(noteObject))
-    // tyhjentaa syotekenttaa kontrolloivan tilan
-    setNewNote('')
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
 
   // tapahtumankasittelija lomakkeen input-komponentin
@@ -44,6 +41,44 @@ const App = (props) => {
     console.log(event.target.value) // viittaa syotekentan arvoon
     setNewNote(event.target.value)
   }
+
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id) // etsitaan muutettava muistiinpano
+
+    /* 
+    - maaritellaan uusi olio, jonka sisalto sama kuin vanhan note-olion poislukien
+      important-kentta
+    - ...note luo olion jolla on kenttinaan kopiot note-olion kenttien arvoista
+    - sitten important-kenttaan tulee vanhan kaanteisarvo
+    - suositeltavaa on nimenomaan tehda oliosta kopio sita muokattaessa, koska muuten
+      muokataan notesia joka on TILA, eli sita ei saa muuttaa suoraan */
+    const changedNote = { ...note, important: !note.important }
+
+    /* PUT-pyynnolla korvataan vanha muistiinpano palvelimella */
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+
+      /* luodaan uusi taulukko vanhan taulukon perusteella
+         Jos note.id !== id on tosi, otetaan uuteen taulukkoon suoraan vanhan
+         taulukon kyseinen alkio. Jos epatosi, eli kyseessa on muutettu muistiinpano,
+         otetaan uuteen taulukkoon palvelimen palauttama olio (joka on tassa tapauksessa
+         muutettu, koska PUT-metodi on juuri muutoksen tehnyt) */
+      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+    })
+    .catch(error => {
+      alert(
+        `the note '${note.content}' was already deleted from server`
+      )
+      /* Olematon muistiinpano poistetaan filterilla, jonka luoman uuden taulukon
+         sisalloksi tulee alkuperaisen taulukon sisallosta ne alkiot, joille
+         parametrina oleva funktio palauttaa arvon true 
+         ...kaytannossa hylkaa vain sen jonka id on tama kasittelyssa oleva id */
+      setNotes(notes.filter(n => n.id !== id))
+    })
+  }
+
 
   const notesToShow = showAll
     ? notes
@@ -59,7 +94,11 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map((note, i) => 
-          <Note key={i} note={note} />
+          <Note 
+            key={i} 
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
--- osa2/esimerkki/src/index.js
+++ osa2/esimerkki/src/index.js
@@ -2,6 +2,4 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './App'
 
-import axios from 'axios'
-
 ReactDOM.render(<App />, document.getElementById('root'))
