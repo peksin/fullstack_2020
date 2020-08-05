d7ea8cdefca25024c932a423a3406740df071248
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-05 12:54:23 +0300

    esimerkin backend omaksi repokseen

--- osa3/helloworld/index.js
+++ osa3/helloworld/index.js
@@ -107,7 +107,7 @@ app.delete('/api/notes/:id', (request, response) => {
 app.use(unknownEndpoint)
 
 // Palvelin kayntiin
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
