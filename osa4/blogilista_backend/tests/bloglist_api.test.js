b154388b7c099d867527e823e9a5497499c9dfc5
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-24 19:17:08 +0300

    osa4 teht 4.9

--- osa4/blogilista_backend/tests/bloglist_api.test.js
+++ osa4/blogilista_backend/tests/bloglist_api.test.js
@@ -19,11 +19,31 @@ test('the right amount of JSON-blogs is returned', async () => {
   console.log(`got ${response.body.length} blogs`)
 })
 
+// id-kentta oikein
 test('id-field of returned blogs is actually id, not _id', async () => {
   const response = await api.get('/api/blogs')
   const contents = response.body.map(r => r.id)
   console.log(contents)
   expect(contents).toBeDefined()
+})
+
+// POST-pyynnon testi
+test('blogs are added correctly', async () => {
+  const newBlog = {
+    _id: "5a421aa71q54a676234d17f8",
+    title: "testiblogi",
+    author: "Edsger W. Testaaja",
+    url: "http://www.fi",
+    likes: 5000,
+    __v: 0
+  }
+
+  const initialLength = helper.initialBlogs.length
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
 })
 
 afterAll(() => {
