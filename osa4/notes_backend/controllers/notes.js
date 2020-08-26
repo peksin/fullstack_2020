7db535a85e4c051009a0e28381a02216f061afa2
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-26 12:38:36 +0300

    osa4 teht. 4.15

--- osa4/notes_backend/controllers/notes.js
+++ osa4/notes_backend/controllers/notes.js
@@ -1,4 +1,5 @@
 const notesRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Note = require('../models/note')
 const User = require('../models/user')
 
@@ -20,9 +21,22 @@ notesRouter.get('/:id', async (request, response) => {
 
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
