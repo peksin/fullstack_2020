feba223ee508fbab63d3395ab7a3e17213fb5ced
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-27 15:47:54 +0300

    osa5 teht. 5.3

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -9,6 +9,9 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [newBlogTitle, setNewBlogTitle] = useState('')
+  const [newBlogAuthor, setNewBlogAuthor] = useState('')
+  const [newBlogUrl, setNewBlogUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -53,15 +56,26 @@ const App = () => {
 
   const handleLogout = async (event) => {
     event.preventDefault()
-    
-    // set user to null
     setUser(null)
-
-    // clear token
     blogService.setToken(null)
-
-    // clear local storage
     window.localStorage.clear()
+  }
+
+  const handleCreateBlog = async (event) => {
+    event.preventDefault()
+
+    const blogObject = {
+      title: newBlogTitle,
+      author: newBlogAuthor,
+      url: newBlogUrl
+    }
+
+    const returnedBlog = await blogService
+      .create(blogObject)
+    setBlogs(blogs.concat(returnedBlog))
+    setNewBlogAuthor('')
+    setNewBlogTitle('')
+    setNewBlogUrl('')
   }
 
 
@@ -92,12 +106,52 @@ const App = () => {
     </>
   )
 
+
+  const createNewBlog = () => (
+    <>
+    <h2>create new</h2>
+    <form onSubmit={handleCreateBlog}>
+      <div>
+        title
+          <input
+          type="text"
+          value={newBlogTitle}
+          name="title:"
+          onChange={({ target }) => setNewBlogTitle(target.value)}
+          />
+      </div>
+      <div>
+        author
+          <input
+          type="text"
+          value={newBlogAuthor}
+          name="author:"
+          onChange={({ target }) => setNewBlogAuthor(target.value)}
+          />
+      </div>
+      <div>
+        url
+          <input
+          type="text"
+          value={newBlogUrl}
+          name="url:"
+          onChange={({ target }) => setNewBlogUrl(target.value)}
+          />
+      </div>
+      <button type="submit">create</button>
+    </form>
+    </>
+  )
+
+
   const blogList = () => (
     <div>
     <h2>blogs</h2>
     <p>{user.username} logged in 
       <button onClick={handleLogout}>logout</button>
     </p>
+
+    {createNewBlog()}
 
     {blogs.map(blog =>
       <Blog key={blog.id} blog={blog} />
