8d1dc0d5d670588ce637348ce12c77572bb2f41d
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-29 13:17:45 +0300

    osa5 teht. 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -3,45 +3,57 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test("renders the blog's title and author, but doesn't render url or likes by default", () => {
-  const user = {
-    username: 'tuntematon'
-  }
+describe('<Blog />', () => {
+  let component
+  let blog
+  let user
 
-  const blog = {
-    title: 'testblog7',
-    author: 'blogger influencer',
-    url: 'http://www.example.com',
-    likes: '9001',
-    user: {
-      username: 'anon'
+
+  beforeEach(() => {
+    user = {
+      username: 'tuntematon'
     }
-  }
 
-  const component = render(
-    <Blog blog={blog} user={user} />
-  )
+    blog = {
+      title: 'testblog7',
+      author: 'blogger influencer',
+      url: 'http://www.example.com',
+      likes: '9001',
+      user: {
+        username: 'anon'
+      }
+    }
 
-  // component.debug()
+    component = render(
+      <Blog blog={blog} user={user} />
+    )
+  })
 
-  const invisibleContent = component.container.querySelector('.togglableContent')
 
-  // testing testing
-  expect(component.container).not.toHaveTextContent(
-    'ei sinne painkaan'
-  )
+  test("renders the blog's title and author, but doesn't render url or likes by default", () => {
+    const invisibleContent = component.container.querySelector('.togglableContent')
+  
+    expect(component.container).not.toHaveTextContent(
+      'ei sinne painkaan'
+    )
+  
+    expect(component.container).toHaveTextContent(
+      'testblog7'
+    )
+  
+    expect(component.container).toHaveTextContent(
+      'blogger influencer'
+    )
+  
+    // url and likes not visible
+    expect(invisibleContent).toHaveStyle('display: none')
+  })
 
-  // title visible
-  expect(component.container).toHaveTextContent(
-    'testblog7'
-  )
-
-  // author visible
-  expect(component.container).toHaveTextContent(
-    'blogger influencer'
-  )
-
-  // url and likes not visible
-  expect(invisibleContent).toHaveStyle('display: none')
-
+  test("renders blog's title, author, url and likes when view-button is pressed", () => {
+    const button = component.getByText('view')
+    fireEvent.click(button)
+    const div = component.container.querySelector('.togglableContent')
+    expect(div).not.toHaveStyle('display: none')
+  })
 })
+
