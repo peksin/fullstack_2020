91c3fa8df9d5a4fd98dbfcfea30edc22c663d219
Pekka Sinkkonen <peksin@protonmail.com>
2020-08-30 23:35:31 +0300

    osa5 teht. 5.18

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,11 +1,38 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     // Tanne tehtavan koodia sit
     cy.contains('log in to application')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('mluukkai logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('maluukkai')
+      cy.get('#password').type('slaine')
+      cy.get('#login-button').click()
+
+      cy.get('.notification')
+        .should('contain', 'wrong username or password')
+      
+      cy.get('html').should('not.contain', 'mluukkai logged in')
+    })
   })
 })
