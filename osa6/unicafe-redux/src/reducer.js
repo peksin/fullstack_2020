fa8dcc54fa4d7dcb454486633ae70f1fce41ee3c
Pekka Sinkkonen <peksin@protonmail.com>
2020-09-01 22:44:14 +0300

    osa6 teht 6.1

---
+++ osa6/unicafe-redux/src/reducer.js
@@ -0,0 +1,36 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+      return {
+        ...state, 
+        good: initialState.good + 1
+      }
+    case 'OK':
+      return {
+        ...state,
+        ok: initialState.good + 1
+      }
+    case 'BAD':
+      return {
+        ...state,
+        bad: initialState.bad + 1
+      }
+    case 'ZERO':
+      return {
+        good: 0,
+        ok: 0,
+        bad: 0
+      }
+    default: return state
+  }
+  
+}
+
+export default counterReducer
