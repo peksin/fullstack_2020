30adb23ce5fe20f069184f141397c0eb18626a3a
Pekka Sinkkonen <peksin@protonmail.com>
2020-09-02 21:34:18 +0300

    osa6 teht 6.9

--- osa6/notes-redux/src/components/Notes.js
+++ osa6/notes-redux/src/components/Notes.js
@@ -13,15 +13,22 @@ const Note = ({ note, handleClick }) => {
 
 const Notes = () => {
   const dispatch = useDispatch()
-  const notes = useSelector(state => state)
+  const notes = useSelector(({ filter, notes }) => {
+    if ( filter === 'ALL' ) {
+      return notes
+    }
+    return filter === "IMPORTANT"
+      ? notes.filter(note => note.important)
+      : notes.filter(note => !note.important)
+  })
 
-  return (
+  return(
     <ul>
       {notes.map(note =>
         <Note
           key={note.id}
           note={note}
-          handleClick={() =>
+          handleClick={() => 
             dispatch(toggleImportanceOf(note.id))
           }
         />
--- osa6/notes-redux/src/index.js
+++ osa6/notes-redux/src/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
 import { Provider } from 'react-redux'
 import App from './App';
-import noteReducer from './reducers/noteReducer'
 
-const store = createStore(noteReducer)
+import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(
+  reducer,
+  composeWithDevTools())
+
+export default store
+
+console.log(store.getState())
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-);
+)
